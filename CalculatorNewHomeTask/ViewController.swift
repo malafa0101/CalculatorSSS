d9909b9909e77bf987814cb3af172fb12606f270
Жанибек <zanibek@MacBook-Pro-Zanibek.local>
2018-01-30 19:20:48 +0600

    Initial Commit

---
+++ CalculatorNewHomeTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorNewHomeTask
+//
+//  Created by Жанибек on 30.01.18.
+//  Copyright © 2018 Жанибек. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
